bbe7d9dc9e7d0de71130bebec404a0bcf868c475
PHLin199988 <yk40403@gmail.com>
2022-06-05 20:11:31 -0400

    initial commit

---
+++ Server/Routes/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+var router = express.Router();
+
+/* Display home page. */
+router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'Home', page:'home' });
+});
+
+/* Display home page. */
+router.get('/home', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'Home', page:'home' });
+});
+
+/* Display about page. */
+router.get('/about', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'About Me', page:'about' });
+});
+
+/* Display projects page. */
+router.get('/projects', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'My Projects', page:'projects' });
+});
+
+/* Display services page. */
+router.get('/services', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'My Services', page:'services' });
+});
+
+/* Display contact page. */
+router.get('/contact', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.render('index', { title: 'Contact Us', page:'contact' });
+});
+export default router;
