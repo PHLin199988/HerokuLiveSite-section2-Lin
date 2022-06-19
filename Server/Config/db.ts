0e22fbd703370b152d552d37304b78b3fab2dcb7
PHLin199988 <yk40403@gmail.com>
2022-06-19 18:07:56 -0400

    updated project to connect to MongoDB Atlas

---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/media";
+export const RemoteURI ="mongodb+srv://PHLin:U4kOsu3iKyAhFXOi@cluster0.emrpo.mongodb.net/media?retryWrites=true&w=majority"
+export const HostName = "MongoDB Atlas";
+export const Secret = "someSecret";
